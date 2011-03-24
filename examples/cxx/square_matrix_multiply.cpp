42c5e086c2299ff0431fd6bfcb0b561f87036b5c
pkambadu <pkambadu@coin-or.clemson.edu>
2011-03-24 19:56:06 +0000

    Finishing the example up --- now square matrix multiplication completely works

--- examples/cxx/square_matrix_multiply.cpp
+++ examples/cxx/square_matrix_multiply.cpp
@@ -57,11 +57,14 @@
  * (3) 'nqueues': Number of queues to create
  * (4) 'nthreads': Number of threads PER QUEUE. Total number of threads is 
  *                 'nqueues' * 'nthreads'.
+ * (5) 'print': The value > 0 results in A, B, and C being printed.
  */
 #include <iostream>
 #include <cstdlib>
+#include <cassert>
 #include <vector>
 #include <pfunc/pfunc.hpp>
+#include <pfunc/utility.h>
 
 /**
  * First, let us get the definition of the PFunc instance.
@@ -76,6 +79,16 @@ typedef generator_type::task task;
 typedef generator_type::group group;
 typedef generator_type::taskmgr taskmgr;
 typedef generator_type::functor functor;
+
+/**
+ * Three global variables:
+ * (1) 'n'
+ * (2) 'x'
+ * (3) 'print'
+ */
+static int problem_dim = 4; /* default 4x4 */
+static int problem_base_case_dim = 4; /* default to problem_dim */
+static bool please_print = false; /* do not print */
 
 /**
  * Returns a random double in the range (0.0,1.0]. Notice that the random 
@@ -113,22 +126,26 @@ struct divisible_matrix {
   int dim;
   int stride;
   int start_index;
+  bool transposed;
   std::vector<double>& matrix;
 
   public:
+
   /**
    * Constructor. 
    * @param[in] dim Dimension of this matrix
    * @param[in] stride Stride to access succesive rows of this matrix.
    * @param[in] start_index Starting point for this matrix.
+   * @param[in] transposed True if this is a transposed representation.
    * @param[in] matrix Reference to the vector that holds the elements.
    */
   divisible_matrix (const int dim,
                     const int stride,
                     const int start_index,
-                    std::vector<double>& matrix) :
-    dim (dim), stride(stride), start_index (start_index), matrix (matrix) {}
-
+                    std::vector<double>& matrix,
+                    const bool transposed = false):
+    dim (dim), stride(stride), start_index (start_index), 
+    matrix (matrix), transposed (transposed) {}
   /**
    * Return a new matrix, which represents the current matrix's quadarant.
    * Quadrants are numbered as shown here: Q1 | Q2
@@ -138,13 +155,16 @@ struct divisible_matrix {
    */
   divisible_matrix split (const int quadrant) const {
     const int new_dim = dim/2;
-    const int new_stride = stride/2;
     const int new_start_index = start_index + 
                                 (1==quadrant) ? 0 :
                                   (2==quadrant) ? new_dim :
                                     (3==quadrant) ? (dim*dim)/2:
                                       (dim*dim)/2 + new_dim;
-    return divisible_matrix (new_dime, new_stride, new_start_index, matrix);
+    return divisible_matrix (new_dim, 
+                             stride, 
+                             new_start_index, 
+                             matrix,
+                             transposed);
   }
 
   /**
@@ -153,9 +173,10 @@ struct divisible_matrix {
    * @param[in] j The column number [0,dim)
    * @return [i][j]th element.
    */
-  double operator[][] (int i, int j) const {
+  double operator() (int i, int j) const {
     assert (i<dim && j<dim);
-    return matrix[start_index + (i*stride) + j];
+    return (transposed) ? 
+      matrix[start_index+(j*stride)+i]:matrix[start_index+(i*stride)+j];
   }
 
   /**
@@ -164,15 +185,33 @@ struct divisible_matrix {
    * @param[in] j The column number [0,dim)
    * @return [i][j]th element.
    */
-  double& operator[][] (int i, int j) {
+  double& operator() (int i, int j) {
     assert (i<dim && j<dim);
-    return matrix[start_index + (i*stride) + j];
+    return (transposed) ? 
+      matrix[start_index+(j*stride)+i]:matrix[start_index+(i*stride)+j];
   }
 
   /**
    * @return Dimension of the matrix
    */
   int dimension () const { return dim; }
+
+  /**
+   * Pretty-print the matrix
+   */
+  void pretty_print () const {
+    // Do some formatting
+    std::ios_base::fmtflags original_flags = std::cout.flags();
+    std::cout.precision(2);
+    std::cout.setf (std::ios_base::fixed, std::ios_base::floatfield);
+    for (int i=0; i<dim; ++i) {
+      for (int j=0; j<dim; ++j) {
+        std::cout << " " << (*this)(i,j);
+      }
+      std::cout << std::endl;
+    }
+    std::cout.flags(original_flags);
+  }
 };
 
 /**
@@ -190,9 +229,9 @@ static inline void dgemm_three_loop (const divisible_matrix& A,
     for (int j=0; j<dim; ++j) {
       register double sum=0.0;
       for (int k=0; k<dim; ++k) {
-        sum += A[i][k]*B[k][j];
+        sum += A(i,k)*B(k,j);
       }
-      C[i][j] += sum;
+      C(i,j) += sum;
     }
   }
 }
@@ -207,7 +246,6 @@ struct dgemm_op {
   const divisible_matrix& A; 
   const divisible_matrix& B; 
   divisible_matrix& C; 
-  int base_case_dim;
 
   public:
   /**
@@ -215,13 +253,10 @@ struct dgemm_op {
    * @param[in] A The submatrix A
    * @param[in] B The submatrix B
    * @param[in] C The submatrix C, which has been initialized
-   * @param[in] base_case_dim Dimension at which we directly multiply.
    */
    dgemm_op (const divisible_matrix& A,
              const divisible_matrix& B,
-             divisible_matrix& C,
-             const int base_case_dim) : 
-     A (A), B (B), C (C), base_case_dim (base_case_dim) {}
+             divisible_matrix& C) : A (A), B (B), C (C) {}
 
   /**
    * Check if we need to recurse any further. If so, spawn additional tasks.
@@ -231,7 +266,7 @@ struct dgemm_op {
     assert (A.dimension() == B.dimension() == C.dimension());
 
     // Multiply directly if we have small enough matrices
-    if (A.dimension() <= base_case_dim) {
+    if (A.dimension() <= problem_base_case_dim) {
       dgemm_three_loop (A, B, C);
     } else { // Divide into four quadrants and recurse.
       // Split the matrices
@@ -254,14 +289,14 @@ struct dgemm_op {
       // -----|----- =  -----|------  *  -----|------
       // q3_C | q4_C    q3_A | q4_A      q3_B | q4_B
       //
-      dgemm_op q1_op_1 (q1_A, q1_B, q1_C, base_case_dim);
-      dgemm_op q1_op_2 (q2_A, q1_B, q1_C, base_case_dim);
-      dgemm_op q2_op_1 (q1_A, q2_B, q2_C, base_case_dim);
-      dgemm_op q2_op_2 (q2_A, q2_B, q2_C, base_case_dim);
-      dgemm_op q3_op_1 (q3_A, q3_B, q3_C, base_case_dim);
-      dgemm_op q3_op_2 (q4_A, q3_B, q3_C, base_case_dim);
-      dgemm_op q4_op_1 (q3_A, q4_B, q4_C, base_case_dim);
-      dgemm_op q4_op_2 (q4_A, q4_B, q4_C, base_case_dim);
+      dgemm_op q1_op_1 (q1_A, q1_B, q1_C);
+      dgemm_op q1_op_2 (q2_A, q1_B, q1_C);
+      dgemm_op q2_op_1 (q1_A, q2_B, q2_C);
+      dgemm_op q2_op_2 (q2_A, q2_B, q2_C);
+      dgemm_op q3_op_1 (q3_A, q3_B, q3_C);
+      dgemm_op q3_op_2 (q4_A, q3_B, q3_C);
+      dgemm_op q4_op_1 (q3_A, q4_B, q4_C);
+      dgemm_op q4_op_2 (q4_A, q4_B, q4_C);
 
 
       // Create the task objects
@@ -290,3 +325,69 @@ struct dgemm_op {
     }
   }
 };
+
+/**
+ * main loop of this example. Reads in all the values, creates the matrices,
+ * multiplies them and finally prints the results if needed.
+ */
+int main (int argc, char** argv) {
+  // All inputs must be given. Else, barf.
+  if (6 != argc) {
+    std::cout << "Please use this program as follows" << std::endl
+              << "./square_matrix_multiply <n> <x> <nqueues> <nthreads> "
+              << "<print(0|1)>" << std::endl;
+    exit(3);
+  }
+
+  problem_dim = get_closest_power_of_2 (atoi(argv[1]));
+  problem_base_case_dim = atoi(argv[2]);
+  assert (problem_dim >= problem_base_case_dim);
+  const unsigned int nqueues = static_cast<unsigned int>(atoi(argv[3]));
+  const unsigned int threads_per_queue = 
+                               static_cast<unsigned int>(atoi(argv[4]));
+  unsigned int* threads_per_queue_array = new unsigned int [nqueues];
+  for (int i=0; i<nqueues; ++i) threads_per_queue_array[i] = threads_per_queue;
+  please_print = static_cast<bool>(atoi(argv[5]));
+
+  // Create the matrices
+  const int num_elements = problem_dim*problem_dim;
+  std::vector<double> A_storage (num_elements);
+  std::vector<double> B_storage (num_elements);
+  std::vector<double> C_storage (problem_dim*problem_dim, 0.0);
+
+  for (int i=0; i<num_elements; ++i) {
+    A_storage[i] = get_next_rand();
+    B_storage[i] = get_next_rand();
+  }
+
+  // Set up the matrices
+  divisible_matrix A (problem_dim,problem_dim,0,A_storage);
+  divisible_matrix B (problem_dim,problem_dim,0,B_storage, true/*transposed*/);
+  divisible_matrix C (problem_dim,problem_dim,0,C_storage);
+
+  // Initialize PFunc
+  taskmgr global_taskmgr (nqueues, threads_per_queue_array);
+  pfunc::init (global_taskmgr);
+
+  // Spawn the root task
+  task root_task;
+  attribute root_attribute (false /*nested*/, false /*grouped*/);
+  dgemm_op root_dgemm (A, B, C);
+  double time = micro_time();
+  pfunc::spawn (root_task, root_attribute, root_dgemm);
+  pfunc::wait (root_task);
+  time = micro_time() - time;
+
+  std::cout << "Multiplication of two " << problem_dim << "x" << problem_dim 
+            << " random matrices took " << time << " seconds" << std::endl;
+
+  if (please_print) {
+    A.pretty_print ();
+    std::cout << std::endl;
+    B.pretty_print ();
+    std::cout << std::endl;
+    C.pretty_print ();
+  }
+
+  return 0;
+}
