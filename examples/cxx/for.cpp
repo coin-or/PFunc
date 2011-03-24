6774dbd02592ccebc8b5babe46918e77a5122802
pkambadu <pkambadu@coin-or.clemson.edu>
2011-03-24 21:55:43 +0000

    Adding a space_1D object that does binary splitting

--- examples/cxx/for.cpp
+++ examples/cxx/for.cpp
@@ -56,3 +56,66 @@
  * very beginning. In fact, this is the model used in MPI-style algorithms and
  * can be mimiced in PFunc using GROUP structures.
  */
+#include <iostream>
+#include <cassert>
+
+/**
+ * A structure that implements a 1-D iteration space --- [begin, end).
+ */
+struct space_1D {
+  static size_t base_case_size = 1; /**< Default which we will over-ride */
+  private:
+  size_t space_begin; /**< Beginning of the iteration space */
+  size_t space_end; /**< End of the iteration space */
+  bool splittable; /**< Shortcut that tells us if we are splittable */
+
+  public:
+  /**
+   * Constructor.
+   * @param[in] begin Beginning of the iteration space.
+   * @param[in] end End of the iteration space.
+   */
+  space_1D (const size_t begin, const size_t end) : 
+    space_begin(space_begin), space_end(space_end), 
+    splittable ((end-begin)>base_case_size) {}
+
+  /**
+   * Get the beginning of the iteration space.
+   * @return Beginning of the iteration space.
+   */
+  size_t begin () const { return space_begin; }
+
+  /**
+   * Get the end of the iteration space.
+   * @return End of the iteration space.
+   */
+  size_t end () const { return space_end; }
+
+  /**
+   * Check if the space is splittable
+   * @return true iff splittable, false otherwise.
+   */
+  bool can_split () const { return splittable; }
+
+  /**
+   * Split the current space into two pieces.
+   * @return A new space that contains atmost half the current iteration space.
+   *         Also, current space is reduced to half its original space.
+   */
+  space_1D split () { 
+    assert (splittable);
+    
+    size_t old_end = end;
+    end = (begin-end)/2;
+    splittable = ((end-begin)>base_case_size);
+    return space_1D (end, old_end);
+  }
+
+  /**
+   * Pretty print
+   */
+  void pretty_print () const {
+    std::cout << "[" << begin << "," << end << ") -- " 
+              << (splittable) ? "splittable" : "NOT splittable" << std::endl;
+  }
+};
